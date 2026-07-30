689d6da5ee247b83d416c46568a16f3b59956978
Hansel <hauwmgames@gmail.com>
2026-07-30 14:04:22 +0100

    feat(client): implement login screen and layout structure

---
+++ client/app/(auth)/login.tsx
@@ -0,0 +1,58 @@
+import { View, Text, Pressable } from "react-native";
+import { Flame, Music, RotateCcwClock, Turntable } from "lucide-react-native";
+
+export default function Login() {
+	return (
+		<View className="bg-background flex-1 items-center justify-center px-8 py-12 gap-20">
+			{/* Logo mark */}
+			<View className="rounded-3xl -mb-4">
+				<Turntable color="#4b2e2b" size={100} />
+			</View>
+
+			{/* Title + tagline */}
+			<View className="items-center -mt-14 gap-2">
+				<Text className="font-serif text-5xl text-text">Listenrr</Text>
+				<Text className="font-outfit text-text-sub text-lg text-center">
+					Your listening life, beautifully mapped
+				</Text>
+			</View>
+
+			{/* Feature hints */}
+			<View className="self-stretch items-start gap-2 px-2">
+				<View className="flex flex-row items-center gap-4">
+					<Music color={"#4b2e2b"} size={20} />
+					<Text className="font-outfit text-text-sub text-base">
+						Exact play count for every track
+					</Text>
+				</View>
+
+				<View className="flex flex-row items-center gap-4">
+					<RotateCcwClock color={"#4b2e2b"} size={20} />
+					<Text className="font-outfit text-text-sub text-base">
+						Release-year timeline of your taste
+					</Text>
+				</View>
+
+				<View className="flex flex-row items-center gap-4">
+					<Flame color={"#4b2e2b"} size={20} />
+					<Text className="font-outfit text-text-sub text-base">
+						Listening activity heatmaps & streaks
+					</Text>
+				</View>
+			</View>
+
+			{/* CTA + footer */}
+			<View className="self-stretch gap-4">
+				<Pressable className="bg-accent py-5 px-8 w-full items-center rounded-3xl active:opacity-80 hover:opacity-80">
+					<Text className="font-outfit-medium text-surface text-xl">
+						Continue with Spotify
+					</Text>
+				</Pressable>
+
+				<Text className="font-outfit text-muted text-center text-sm">
+					Read-only. Your data stays yours.
+				</Text>
+			</View>
+		</View>
+	);
+}
